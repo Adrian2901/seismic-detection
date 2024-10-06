4399eef94fa9ed79bbd5686e3aca68a12733cc6b
Adrian Hassa <adrianhassa@gmail.com>
2024-10-06 15:40:07 +0200

    Clean up the filter algorithm

--- filter.py
+++ filter.py
@@ -9,25 +9,17 @@ def filter(file_st):
     # Copy the original stream to avoid modifying the original data
     st_threshold  = file_st.copy()
     trace = st_threshold.traces[0]
-    sampling_rate = trace.stats.sampling_rate
-    starttime = trace.stats.starttime
     
-    #TODO: return sampling_rate, starttime, and trace
-
-    # Get the velocity data
-    npts = trace.stats.npts
-    times = trace.times('timestamp')
     velocities = trace.data
 
     # Create a DataFrame with time and velocity data
     df = pd.DataFrame({
-        'time_abs': pd.to_datetime(times, unit='s', origin=pd.Timestamp(starttime.datetime)),
         'velocity(m/s)': velocities
     })
 
     df['abs_velocity(m/s)'] = np.abs(df['velocity(m/s)'])
 
-    # Calculate the threshold based on the 0.25 percent of the max velocity
+    # Calculate the threshold based on the 20 percent of the max velocity
     threshold = 0.2 * np.max(df['abs_velocity(m/s)'])
 
     # Filter out rows where velocity is below a certain threshold
@@ -36,7 +28,6 @@ def filter(file_st):
 
     # Filter out rows where absolute velocity is below a certain threshold
     # Replace velocities below the threshold with the default value
-    df['filtered_velocity(m/s)'] = np.where(df['abs_velocity(m/s)'] >= threshold, df['abs_velocity(m/s)'], default_value)
     filtered_velocities = np.where(df['abs_velocity(m/s)'] >= threshold, df['abs_velocity(m/s)'], default_value)
 
     # Use a sliding window to get the widest range of continuous data
@@ -62,7 +53,6 @@ def filter(file_st):
                 current_zeroes = 0
                 current_length = 0
                 
-    print('Longest sequence: ', max_length, 'from', longest_start, 'to', longest_end)
     
     # Prepare the result array with original size
     result_velocities = np.full_like(filtered_velocities, default_value, dtype=float)
@@ -70,9 +60,5 @@ def filter(file_st):
     # Fill up the data
     if max_length:
         result_velocities[longest_start:longest_end+1] = filtered_velocities[longest_start:longest_end+1]
-    
-    # Add the filtered data to the DataFrame
-    df['longest_consecutive_velocity(m/s)'] = result_velocities
-    print(df['longest_consecutive_velocity(m/s)'])
 
-    return df
+    return result_velocities
