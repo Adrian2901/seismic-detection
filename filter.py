6c1ecc3e2850d364b8cc25e1f638a1ff9b826102
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-10-06 13:54:21 +0200

    Update filters

--- filter.py
+++ filter.py
@@ -5,35 +5,14 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import os
 
-# # TODO: Change this for the testing data
-# # Get the catalogue to find the actual arrival time
-# print('Reading catalogue...\n')
-
-# cat_directory = './space_apps_2024_seismic_detection/data/lunar/training/catalogs/'
-# cat_file = cat_directory + 'apollo12_catalog_GradeA_final.csv'
-# cat = pd.read_csv(cat_file)
-
-# # Choose a file from the catalogue
-# # row = cat.iloc[6]
-# row = cat.iloc[9]
-# print('Choosing file:', row.filename)
-# print('')
-# arrival_time = datetime.strptime(row['time_abs(%Y-%m-%dT%H:%M:%S.%f)'],'%Y-%m-%dT%H:%M:%S.%f')
-
-# # Read the mseed file
-# test_filename = row.filename
-# data_directory = './space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA/'
-# mseed_file = f'{data_directory}{test_filename}.mseed'
-# print('Reading mseed file:', mseed_file)
-# print('')
-# st = read(mseed_file)
-
 def filter(file_st):
     # Copy the original stream to avoid modifying the original data
     st_threshold  = file_st.copy()
     trace = st_threshold.traces[0]
     sampling_rate = trace.stats.sampling_rate
     starttime = trace.stats.starttime
+    
+    #TODO: return sampling_rate, starttime, and trace
 
     # Get the velocity data
     npts = trace.stats.npts
@@ -58,30 +37,42 @@ def filter(file_st):
     # Filter out rows where absolute velocity is below a certain threshold
     # Replace velocities below the threshold with the default value
     df['filtered_velocity(m/s)'] = np.where(df['abs_velocity(m/s)'] >= threshold, df['abs_velocity(m/s)'], default_value)
+    filtered_velocities = np.where(df['abs_velocity(m/s)'] >= threshold, df['abs_velocity(m/s)'], default_value)
+
+    # Use a sliding window to get the widest range of continuous data
+    longest_start = longest_end = current_start = 0
+    max_length = 0
+    current_length = 0
+    current_zeroes = 0
+    max_number_of_zeroes = 50000
+    
+    for i in range(len(filtered_velocities)):
+        if filtered_velocities[i] > default_value:
+            if current_length == 0:
+                current_start = i # Start a new sequence
+            current_length += 1
+            
+            if current_length > max_length:
+                max_length = current_length
+                longest_start = current_start
+                longest_end = i
+        else:
+            current_zeroes += 1
+            if (current_zeroes > max_number_of_zeroes):
+                current_zeroes = 0
+                current_length = 0
+                
+    print('Longest sequence: ', max_length, 'from', longest_start, 'to', longest_end)
+    
+    # Prepare the result array with original size
+    result_velocities = np.full_like(filtered_velocities, default_value, dtype=float)
+    
+    # Fill up the data
+    if max_length:
+        result_velocities[longest_start:longest_end+1] = filtered_velocities[longest_start:longest_end+1]
+    
+    # Add the filtered data to the DataFrame
+    df['longest_consecutive_velocity(m/s)'] = result_velocities
+    print(df['longest_consecutive_velocity(m/s)'])
 
     return df
-
-# Create a figure with two subplots (1 row, 2 columns)
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))  # Adjust size as needed
-
-# # Plot original velocities in the first subplot
-# ax1.plot(df['time_abs'], df['velocity(m/s)'], label='Original Velocity', color='blue', alpha=0.7)
-# ax1.set_title('Original Velocity Over Time')
-# ax1.set_xlabel('Time')
-# ax1.set_ylabel('Velocity (m/s)')
-# ax1.legend()
-
-# # Plot filtered velocities in the second subplot
-# # Plot filtered velocities (with default values) in the second subplot
-# ax2.plot(df['time_abs'], df['filtered_velocity(m/s)'], label='Filtered Velocity with Defaults', color='red', linewidth=2)
-# ax2.set_title(f'Filtered Velocity with Default Values (Threshold: {threshold})')
-# ax2.set_xlabel('Time')
-# ax2.set_ylabel('Velocity (m/s)')
-# ax2.legend()
-
-# # Adjust layout to prevent overlap
-# plt.tight_layout()
-
-# # Show the plot
-# plt.show()
-
